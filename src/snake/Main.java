57288b8b3584387aafa96d0c1e522f57cf248849
JuanAntonio <Estudio@10.58.27.24>
2022-03-01 09:55:51 +0100

    documentación realizada

---
+++ src/snake/Main.java
@@ -0,0 +1,22 @@
+package snake;
+
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                MainWindow mw = new MainWindow();
+                mw.setSize(600, 400);
+                mw.setTitle("Snake and Ladder");
+                mw.setVisible(true);
+            }
+        });
+    }
+
+}
